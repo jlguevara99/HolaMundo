94db9431e7d4423de70a384e7dca02a2e1ea7687
Juan Guevara <Juan Guevara>
2017-04-27 19:29:13 -0600

    ya

--- unNombre/src/unnombre/UnNombre.java
+++ unNombre/src/unnombre/UnNombre.java
@@ -16,6 +16,8 @@ public class UnNombre {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        System.out.println("Escribi");
+        System.out.println("lol");
     }
     
 }
